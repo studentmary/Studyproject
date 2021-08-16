56ea4e90e65da8c799e2b167d2e84b4a200523e2
studentmary <val.mary81@yandex.ru>
2021-08-16 17:52:55 +0300

    add Task3

---
+++ course_java/src/by/stormnet/volodko/lesson5/task3/Main.java
@@ -0,0 +1,13 @@
+package by.stormnet.volodko.lesson5.task3;
+
+public class Main {
+    public static void main(String[] args) {
+        move.car car1 = new move.car();
+        car1.run();
+        move.bicycle bicycle1 = new move.bicycle();
+        bicycle1.run();
+        move.snail  snail1 = new move.snail();
+        snail1.run();
+
+    }
+}
---
+++ course_java/src/by/stormnet/volodko/lesson5/task3/move.java
@@ -0,0 +1,23 @@
+package by.stormnet.volodko.lesson5.task3;
+
+public class move {
+
+    public static class car {
+        public void run() {
+            System.out.println("Врум-Врум");
+        }
+
+    }
+    public static class bicycle {
+        public void run() {
+            System.out.println("Шарк-Шарк");
+        }
+
+    }
+    public static  class snail {
+        public void run() {
+            System.out.println("шлеп");
+        }
+    }
+}
+
