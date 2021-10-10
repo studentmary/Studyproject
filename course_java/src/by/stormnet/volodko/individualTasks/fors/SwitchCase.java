6053989c6fb735af270ab7ce13eb6a1064ec36ad
Pavel.Chachotkin <Agentbars00070>
2021-10-10 14:24:07 +0300

    Added task

---
+++ course_java/src/by/stormnet/volodko/individualTasks/fors/SwitchCase.java
@@ -0,0 +1,43 @@
+package by.stormnet.volodko.individualTasks.fors;
+
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
+public class SwitchCase {
+
+	private static final Set<String> months = new HashSet<>();
+
+	static {
+		Collections.addAll(months,
+				"January",
+				"February",
+				"March",
+				"April",
+				"May",
+				"June",
+				"July",
+				"August",
+				"September",
+				"October",
+				"November",
+				"December");
+	}
+
+	public static void main(String[] args) {
+		/*
+		 1. Реализовать предыдущие задания через do while и while
+
+		 2. Даны времени месяца в их строковом представлении
+		 Необходимо вывести на экран номер месяца используя конструкцию Switch Case
+
+		 3. Необходимо вывести время года используя конструкцию Switch Case
+			В случае указания неверного месяца должно выводится собщение о том, что введен не верный месяц (default)
+
+		 */
+
+	}
+
+}
+
